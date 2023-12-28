daa5943b8c6995451084cf205b59048568b08f4b
Ivan Danchenko <danchenko.ivan@inbox.ru>
2023-12-28 22:44:01 +0700

    update: added texture shader

--- Render/Shader/ShaderLibrary.cs
+++ Render/Shader/ShaderLibrary.cs
@@ -1,6 +1,19 @@
 ﻿namespace UniversityGameProject.Render.Shader;
 
-public class ShaderLibrary
+static class ShaderLibrary
 {
-    
+    public static ShaderProgram TextureShader(ShaderContext context)
+    {
+        string vertTextureSource = File.ReadAllText("Render/Shader/ShaderResources/Texture/shaderTexture.frag");
+        string fragTextureSource = File.ReadAllText("Render/Shader/ShaderResources/Texture/shaderTexture.vert");
+
+        Shader vert = new Shader(context, ShaderType.VertexShader, vertTextureSource);
+        Shader frag = new Shader(context, ShaderType.FragmentShader, fragTextureSource);
+
+        ShaderProgram program = new ShaderProgram(context);
+        program.AttachShader(vert);
+        program.AttachShader(frag);
+
+        return program;
+    }
 }
