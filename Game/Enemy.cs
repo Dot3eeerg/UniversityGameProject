a0d41b96c9006016627cae5ee4cb055da03be484
Ivan Danchenko <danchenko.ivan@inbox.ru>
2024-01-04 13:33:26 +0700

    update: enemy can take damage

--- Game/Enemy.cs
+++ Game/Enemy.cs
@@ -47,6 +47,16 @@ public abstract class Enemy : Node2D
         _collision.GlobalTransform.Position = GlobalTransform.Position;
     }
 
+    public void InflictDamage(int damage)
+    {
+        EnemyStats.CurrentHealth -= damage;
+
+        if (EnemyStats.CurrentHealth <= 0)
+        {
+            Console.WriteLine("Enemy is dead");
+        }
+    }
+
     public void DoScale()
     {
         _body.MeshData.ApplyScale(0.04f, 0.08f);
